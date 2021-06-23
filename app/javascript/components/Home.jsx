4c48dd13e6bfac2fc7f51ed16a745211143fd7df
Taha Husain <tahahussain18@gmail.com>
2021-06-23 23:07:39 +0530

    Frontend setup using react - Add home page with bare minimum information

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Peher</h1>
+        <p className="lead">
+          A music player powered by Youtube that plays music based on time of the day.
+        </p>
+        <hr className="my-4" />
+      </div>
+    </div>
+  </div>
+);
