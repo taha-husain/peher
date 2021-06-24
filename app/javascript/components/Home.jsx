70a3e4426dcaa0073f9133cf2e0b2efc329bfb60
Taha Husain <tahahussain18@gmail.com>
2021-06-24 21:44:26 +0530

    Add app's logo

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,11 +5,10 @@ export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">Peher</h1>
+        <img src={"/logo.png"} />
         <p className="lead">
           A music player powered by Youtube that plays music based on time of the day.
         </p>
-        <hr className="my-4" />
         <YouTube
           videoId='Iuv6hY6zsd0'
           id='yt-player'
